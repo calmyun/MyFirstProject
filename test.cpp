e92f9b39b25a10b66f48953de0392dc38ecb7501
calmyun <2430671539@QQ.COM>
2024-09-06 16:37:06 +0800

    update cpp program

--- test.cpp
+++ test.cpp
@@ -1,8 +1,10 @@
-```c++
 #include<iostream>
 int main(){
     std::cout << "hello my git project" << std::endl;
+    for(int i = 0;i < 3;++i)
+	{
+		std::cout << "print something." << std::endl;
+	} 
 	return 0;
 }
-```
 
